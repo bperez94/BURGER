a3b2b1a416a21b60c48f5ef7f616ac9599c943e0
Brandon Perez <brandonp940@gmail.com>
2020-08-12 10:53:30 -0400

    sql connection added, layout for sql querys added top orm

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+var connection = require("../config/connection.js");
+
+// sql querys
+var orm = {
+    all: function selectAll() {
+        
+    },
+
+    create: function insertOne() {
+        
+    },
+
+    update: function updateOne() {
+        
+    }
+}
+
+module.exports = orm;
