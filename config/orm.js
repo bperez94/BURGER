af13eb7dbe5f8241d957cc41144c6e38e081a230
Brandon Perez <brandonp940@gmail.com>
2020-08-17 11:33:37 -0400

    first two methods added, need update method

--- config/orm.js
+++ config/orm.js
@@ -2,17 +2,22 @@ var connection = require("../config/connection.js");
 
 // sql querys
 var orm = {
-    all: function selectAll() {
-        
-    },
-
-    create: function insertOne() {
-        
-    },
-
-    update: function updateOne() {
-        
-    }
+   selectAll: function(tableInput, cb) {
+       connection.query("SELECT * FROM" + tableInput + ";", function(err, result) {
+           if (err) {
+              throw err;
+           }
+           cb(result);
+       })
+   },
+   insertOne: function(table, name, cb) {
+       connection.query("INSERT INTO" + table + "(burger_name, devoured) VALUES (?,0);", [name], function(err,result) {
+           if (err) {
+               throw err;
+           }
+           cb(result);
+       }  )
+   }
 }
 
 module.exports = orm;
