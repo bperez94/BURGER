1e36781be06422e8816f114ac468668387a4ddba
Brandon Perez <brandonp940@gmail.com>
2020-08-23 05:38:52 -0400

    dated files to handel routing and updated array of functions

--- config/orm.js
+++ config/orm.js
@@ -2,22 +2,30 @@ var connection = require("../config/connection.js");
 
 // sql querys
 var orm = {
-   selectAll: function(tableInput, cb) {
-       connection.query("SELECT * FROM" + tableInput + ";", function(err, result) {
+   selectAll: function(table, cb) {
+       connection.query("SELECT * FROM burgers" +  ";"  ,function(err, res) {
            if (err) {
               throw err;
            }
-           cb(result);
+           cb(res);
        })
    },
-   insertOne: function(table, name, cb) {
-       connection.query("INSERT INTO" + table + "(burger_name, devoured) VALUES (?,0);", [name], function(err,result) {
-           if (err) {
-               throw err;
-           }
-           cb(result);
-       }  )
-   }
+   insertOne: function(table , burger_name, cb) {
+       connection.query("INSERT INTO burgers" + table +"(burger_name, devoured) VALUES (?,0);",[name], function(err,res) {
+        if (err) {
+            throw err;
+          }
+          cb(result);
+        }); 
+   },
+   updateOne: function(table, val, id, cb) { 
+    connection.query("UPDATE " + table + " SET devoured = " + val + " WHERE id = " + id + "", [table, val, id], function (err, res) {
+        if (err) {
+            throw err;
+          }
+          cb(res);
+    });
+} 
 }
 
 module.exports = orm;
