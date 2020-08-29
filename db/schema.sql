d80fdb3bf32d9d1ee7cf51e98fe64075a9bf0731
Brandon Perez <brandonp940@gmail.com>
2020-08-29 17:25:41 -0400

    updated querys

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ CREATE TABLE burgers
 (
     id int NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("bacon cheese burger", 1);
-INSERT INTO burgers (burger_name, devoured) VALUES ("veggie burger", 2);
-INSERT INTO burgers (burger_name, devoured) VALUES ("double patty burger",3);
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon cheese burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("double patty burger",true);
